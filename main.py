f9e9b4b7b81c3b2d03bd4895bc77badee9f8ce9a
Eugen <40069502+Meugenn@users.noreply.github.com>
2018-08-12 20:55:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import config
 from bot_token import TOKEN
 import mysql
 from flask import Flask, request
-
+import logging
 
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
 
 
 
@@ -107,18 +106,21 @@ def new_business(message):
 
 # @bot.message_handler(func=lambda message: condition[message.chat.id]  == config.Waiting.WAITING_LIST.value)
 
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+server = Flask(__name__)
+
+
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://telegram-bot-to-do.herokuapp.com/' + TOKEN)
-    return "!", 200
+    return "?", 200
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
